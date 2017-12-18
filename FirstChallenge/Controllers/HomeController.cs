cce2282816af3a10d50ffe4a9729b84a42a3f13f
Travis Brooks <tbrooks3141@gmail.com>
2017-12-18 13:32:32 -0800

    Add project files.

---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comicBooks = FirstChallenge.Models.ComicBookManager.GetComicBooks();
+            return View(comicBooks);
+        }
+
+        // GET: Details
+        public ActionResult Details(int? id)
+        {
+            var comicBooks = FirstChallenge.Models.ComicBookManager.GetComicBooks();
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var comicBook = comicBooks.Find(p => p.ComicBookId == id);
+            if (comicBook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comicBook);
+        }
+    }
+}
